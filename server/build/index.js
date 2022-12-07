32645ed324f22eaac02b0532319617779ab69ee6
NoobChicken2 <lucaklaus921@gmail.com>
2022-12-07 10:24:32 +0100

    Imported Express

--- server/build/index.js
+++ server/build/index.js
@@ -1,4 +1,3 @@
 "use strict";
 console.log('hello world');
 console.log('ts ran');
-console.log('evviva');
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,18 @@
-console.log('hello world');
-console.log('ts ran');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log(req);
+    // res.send('Hello World!')
+    res.json({ msg: "hello world"});
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
