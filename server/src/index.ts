f8fc2f37659b58adc0cff201284a0049436b45d1
adriandkrantz <adriandkrantz@gmail.com>
2022-12-07 09:55:32 +0100

    Outline of CRUD for tickets created

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,13 @@
-console.log('hello world');
-console.log('ts ran');
+const express = require("express");
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
---
+++ server/src/routes/ticket.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const router = express.Router();
+router.use(express.json());
+
+router.get('/', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+router.patch('/:id', async (req, res) => {
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+});
