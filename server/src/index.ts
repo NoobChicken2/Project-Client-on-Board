2df4cdfbd3751b49a6c0251c65c0b01ab61ab628
AdrianKrantz <524202@student.saxion.nl>
2022-12-07 13:03:32 +0100

    imported Cors and Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,18 @@
-const express = require("express");
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
-
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    console.log(req);
+    // res.send('Hello World!')
+    res.json({ msg: "hello world"});
+});
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
