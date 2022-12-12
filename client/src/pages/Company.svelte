043d8719a0fe7ae773fadead6a8c1fbeb4154ce6
BejanDarius <516471@student.saxion.nl>
2022-12-12 15:36:34 +0100

    login

---
+++ client/src/pages/Company.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<body>
+    <div class="container">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>#</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr>
+                <td ></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+<style>
+
+</style>
