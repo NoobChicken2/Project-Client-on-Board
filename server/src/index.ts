40b34ac8207d6134c0e2659c6e83e4e33243bfe7
Georgs <gergjack@gmail.com>
2022-12-13 11:44:44 +0100

    trying to connect to DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import converterRouter from "./routes/converterScript";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/converters',converterRouter);
 
 app.get('/', (req, res) => {
     console.log(req);
