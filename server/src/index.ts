ce9fa167373a8a2facdd3ae22fadc09e7696d834
adriandkrantz <adriandkrantz@gmail.com>
2022-12-14 10:16:05 +0100

    CRUD created for tickets, working with database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import  {ticketRouter}  from './routes/ticket';
+
 
 const app = express();
 const port = 3000;
-const ticketRouter = require('routes/ticket')
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log(req);
-    // res.send('Hello World!')
-    res.json({msg: "hello world"});
-});
-
-app.use('/tickets', ticketRouter);
+app.use('/tickets',ticketRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- server/src/routes/ticket.ts
+++ server/src/routes/ticket.ts
@@ -1,23 +1,74 @@
 import express from 'express'
-const {fetchTableData} = require("../databaseConnection/fetchData");
+export const ticketRouter = express.Router();
+ticketRouter.use(express.json());
 
-const router = express.Router();
-router.use(express.json());
 
-router.get('/', async (req, res) => {
-    return fetchTableData();
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'admin',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'admin123',
+    port: 7000,
+})
+
+
+ticketRouter.get('/', async (req, res) => {
+    pool.query('SELECT * FROM tickets', (error: any, results: { rows: any; }) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
 });
 
-router.post('/', async (req, res) => {
+ticketRouter.get('/:id', async (req, res) => {
+    let id = req.params.id;
+    pool.query(`SELECT *
+                FROM tickets
+                WHERE ticket_id = ${id}`, (error: any, results: { rows: any; }) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+});
+
+ticketRouter.post('/', async (req, res) => {
+    let id = req.body.log_id
+    pool.query(`INSERT INTO tickets (log_id)
+                VALUES (${id})`, (error: any, results: { rows: any; }) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
 
 });
 
-router.patch('/:id', async (req, res) => {
+ticketRouter.patch('/:id', async (req, res) => {
+    let t_id = req.params.id
+    let l_id = req.body.log_id
 
+    pool.query(`UPDATE tickets
+                SET log_id = ${l_id}
+                WHERE ticket_id = ${t_id}`, (error: any, results: { rows: any; }) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
 });
 
-router.delete('/:id', async (req, res) => {
+ticketRouter.delete('/:id', async (req, res) => {
+    let id = req.params.id
 
+    pool.query(`DELETE
+                FROM tickets
+                WHERE ticket_id = ${id}`, (error: any, results: { rows: any; }) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
 });
-
-module.exports = router;
