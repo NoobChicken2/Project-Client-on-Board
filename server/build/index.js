1843472d4fe7bcb0ba8e69c40f2340f80a36b699
493611 <493611@student.saxion.nl>
2022-12-14 17:47:18 +0100

    get the sample crud to run

--- server/build/index.js
+++ server/build/index.js
@@ -1,3 +1,34 @@
 "use strict";
-console.log('hello world');
-console.log('ts ran');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var app = (0, express_1.default)();
+var port = 3000;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get('/', function (req, res) {
+    console.log(req);
+    // res.send('Hello World!')
+    res.json({ msg: "hello world" });
+});
+app.listen(port, function () {
+    console.log("Example app listening on port ".concat(port));
+});
+// copy from converter branch, testing
+var pg_1 = require("pg");
+var client = new pg_1.Client({
+    host: "localhost",
+    user: "admin",
+    password: "admin123",
+    database: "postgres",
+    port: 7000
+});
+client.connect()
+    .then(function () { return console.log("Success"); })
+    .then(function () { return client.query("select * from users where role='CompanyAdmin'"); })
+    .then(function (results) { return console.table(results.rows); })
+    .catch(function (e) { return console.log(e); })
+    .then(function () { return client.end(); });
--- server/src/index.ts
+++ server/src/index.ts
@@ -16,3 +16,21 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
+
+// copy from converter branch, testing
+import {Client} from 'pg';
+
+const client = new Client({
+    host: "localhost",
+    user: "admin",
+    password: "admin123",
+    database: "postgres",
+    port: 7000
+})
+
+client.connect()
+    .then(() => console.log("Success"))
+    .then(() => client.query("select * from users where role='CompanyAdmin'"))
+    .then((results: { rows: any }) => console.table(results.rows))
+    .catch((e: any) => console.log(e))
+    .then(() => client.end())
