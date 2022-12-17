bbfbb6b16a5fd00dc27650eacf9f1612dfef50a9
sadra <519407@student.saxion.nl>
2022-12-17 12:06:53 +0100

    pushing for further merge of the develop branch

--- client/src/Page/StatusConverter.svelte
+++ client/src/Page/StatusConverter.svelte
@@ -1,4 +1,12 @@
 <script>
+    import ConvertersList from "../Components/ConvertersList.svelte";
 </script>
+<body>
+<ConvertersList/>
+</body>
 <style>
+    body {
+        background-color: #ffffff;
+    }
 </style>
+
