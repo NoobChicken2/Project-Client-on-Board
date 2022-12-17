67900e54de6de889adc609bfa72e7ba5bb154d13
493611 <493611@student.saxion.nl>
2022-12-17 02:11:48 +0100

    create CRUD for company admins & test file for http requests

--- server/build/index.js
+++ server/build/index.js
@@ -5,30 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
+var ticket_1 = __importDefault(require("./routes/ticket"));
+var companyAdministrators_1 = __importDefault(require("./routes/companyAdministrators"));
 var app = (0, express_1.default)();
 var port = 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.get('/', function (req, res) {
-    console.log(req);
-    // res.send('Hello World!')
-    res.json({ msg: "hello world" });
-});
+app.use('/tickets', ticket_1.default);
+app.use('/companyAdmins', companyAdministrators_1.default);
 app.listen(port, function () {
     console.log("Example app listening on port ".concat(port));
 });
-// copy from converter branch, testing
-var pg_1 = require("pg");
-var client = new pg_1.Client({
-    host: "localhost",
-    user: "admin",
-    password: "admin123",
-    database: "postgres",
-    port: 7000
-});
-client.connect()
-    .then(function () { return console.log("Success"); })
-    .then(function () { return client.query("select * from users where role='CompanyAdmin'"); })
-    .then(function (results) { return console.table(results.rows); })
-    .catch(function (e) { return console.log(e); })
-    .then(function () { return client.end(); });
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import ticket from './routes/ticket';
-
+import companyAdmin from './routes/companyAdministrators';
 
 const app = express();
 const port = 3000;
@@ -11,25 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/tickets', ticket);
+app.use('/companyAdmins', companyAdmin);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
-
-// copy from converter branch, testing
-import {Client} from 'pg';
-
-const client = new Client({
-    host: "localhost",
-    user: "admin",
-    password: "admin123",
-    database: "postgres",
-    port: 7000
-})
-
-client.connect()
-    .then(() => console.log("Success"))
-    .then(() => client.query("select * from users where role='CompanyAdmin'"))
-    .then((results: { rows: any }) => console.table(results.rows))
-    .catch((e: any) => console.log(e))
-    .then(() => client.end())
