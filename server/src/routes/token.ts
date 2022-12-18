45994b315a7c5d76135476de923652377cc06d76
NoobChicken2 <lucaklaus921@gmail.com>
2022-12-18 20:52:25 +0100

    initial token

---
+++ server/src/routes/token.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+const router = express.Router();
+
+import {compareLoginDetails, tokenBodyDetails, secret} from '../middleware/authorizationMiddleware';
+import jwt from 'jsonwebtoken';
+
+// @ts-ignore
+router.post("/", tokenBodyDetails, compareLoginDetails, (req, res) => {
+    let payload = req.body;
+    payload.date = new Date();
+    // @ts-ignore
+    payload.roles = req.role;
+
+    jwt.sign(payload, secret, {algorithm: 'HS256'}, (err, result) => {
+        if (err) {
+            return res.status(500).json({error: 'Something went wrong with the token'})
+        }
+        // @ts-ignore
+        return res.status(200).json({token: result});
+    });
+
+});
+
+export default router;
