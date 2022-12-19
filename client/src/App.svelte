29e4bf361a1c4fe181ddb14260129068804b18fc
Sadra <519407@student.saxion.nl>
2022-12-19 12:29:49 +0100

    updating the routing of pages because each time i merge it deletes it

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -5,6 +5,7 @@
   import login from './pages/Login.svelte'
   import register from './pages/Customers.svelte'
   import ticket from './pages/Tickets.svelte'
+  import converter from './pages/StatusConverter.svelte'
 
   let page;
 
@@ -19,6 +20,9 @@
   })
   router('/tickets', (ctx) =>{
       page = ticket;
+  })
+  router('/converters', (ctx) => {
+      page = converter;
   })
 
   router.start()
