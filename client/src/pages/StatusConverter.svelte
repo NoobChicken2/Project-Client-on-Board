5bcdd71ed0e409477d4e4e38db7d19feb01bb426
AdrianKrantz <524202@student.saxion.nl>
2023-01-09 19:48:12 +0100

    bug fix for editing ticket in frontend

--- client/src/pages/StatusConverter.svelte
+++ client/src/pages/StatusConverter.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import {onMount} from "svelte";
     import {loadConverters, addConverter, removeConverter, editConverter} from "../scripts/converterScript";
     import NavigationBar from "../Components/NavigationBar.svelte";
@@ -17,8 +17,6 @@
 
     let error;
     let message;
-    let deleteId;
-
     let selectedId;
 
     let data = {
@@ -26,37 +24,51 @@
         installer_id: "",
         expected_throughput: ""
     }
-    let isEdit =(id) => {
-        selectedId = id;
+
+    function isEdit(converterId: number): void {
+        selectedId = converterId;
         showEditPopup = true;
     }
+
     const updateConverter = async () => {
-        let dataToUpdate = {
-            owner_id: parseInt(data.owner_id),
-            installer_id:parseInt(data.installer_id),
-            expected_throughput: data.expected_throughput
-        }
+        type Data = {
+            owner_id: string;
+            installer_id: string;
+            expected_throughput: string;
+        };
+
+        const fieldsToUpdate: Array<keyof Data> = ['owner_id', 'installer_id', 'expected_throughput'];
+        const dataToUpdate: Partial<Data> = {};
+
+        fieldsToUpdate.forEach((field) => {
+            if (data[field] && data[field] !== '') {
+                dataToUpdate[field] = data[field];
+            }
+        });
+
         console.log(dataToUpdate)
         console.log(selectedId)
-        await editConverter(dataToUpdate,selectedId);
+        await editConverter(dataToUpdate, selectedId);
         showEditPopup = false;
         await loadConverters();
     }
-    function deleteConverter(converter_id){
-        deleteId = converter_id;
+
+    function deleteConverter(converterId: number): void {
+        selectedId = converterId;
         showDeletePopup = true;
     }
+
     const execute = async () => {
-        await removeConverter(deleteId);
+        await removeConverter(selectedId);
         showDeletePopup = false;
         await loadConverters();
     }
 
-    function handleAdd(){
+    function handleAdd() {
         error = undefined;
         message = undefined;
-         addConverter(ownerId,installerId,expected_throughput).then((response) => {
-            if (response.error !== undefined){
+        addConverter(ownerId, installerId, expected_throughput).then((response) => {
+            if (response.error !== undefined) {
                 error = response.error
             } else {
                 message = "Converter added!"
@@ -65,7 +77,6 @@
             }
         })
     }
-
 
 
 </script>
@@ -78,14 +89,17 @@
         <form>
             <div class="modal-header">
                 <h5 class="modal-title" id="sampleModalLabel">Delete</h5>
-                <button type="button" class="bi bi-x-circle" data-dismiss="modal" aria-label="Close" on:click={() => showDeletePopup = false}>
+                <button type="button" class="bi bi-x-circle" data-dismiss="modal" aria-label="Close"
+                        on:click={() => showDeletePopup = false}>
                 </button>
             </div>
             <i class="bi bi-x-circle d-flex justify-content-center" style="font-size: 5rem; color: red"></i>
             <br>
             <h3 class="d-flex justify-content-center">Are you sure?</h3>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal" on:click={() => showDeletePopup = false}>Close</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"
+                        on:click={() => showDeletePopup = false}>Close
+                </button>
                 <button type="button" class="btn btn-danger" on:click={() =>execute()}>Delete</button>
             </div>
         </form>
@@ -96,8 +110,9 @@
     <Modal open={showAddPopup} on:click={ () => showEditPopup = false}>
         <form>
             <div class="modal-header">
-                <h5 class="modal-title" >Add</h5>
-                <button type="button" class="bi bi-x-circle" data-dismiss="modal" on:click={ () => showAddPopup = false}>
+                <h5 class="modal-title">Add</h5>
+                <button type="button" class="bi bi-x-circle" data-dismiss="modal"
+                        on:click={ () => showAddPopup = false}>
                 </button>
             </div>
             <div class="modal-body">
@@ -118,7 +133,9 @@
                 {#if message}<p>{message}</p>{/if}
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal"  on:click={ () => showAddPopup = false}>Close</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"
+                        on:click={ () => showAddPopup = false}>Close
+                </button>
                 <button type="button" class="btn btn-success" on:click={() => handleAdd()}>Add</button>
             </div>
 
@@ -131,8 +148,9 @@
     <Modal open={showEditPopup} on:click={() => showEditPopup = false}>
         <form>
             <div class="modal-header">
-                <h5 class="modal-title" >Edit</h5>
-                <button type="button" class="bi bi-x-circle" data-dismiss="modal" on:click={ () => showEditPopup = false}>
+                <h5 class="modal-title">Edit</h5>
+                <button type="button" class="bi bi-x-circle" data-dismiss="modal"
+                        on:click={ () => showEditPopup = false}>
                 </button>
             </div>
             <div class="modal-body">
@@ -150,7 +168,9 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal"  on:click={ () => showEditPopup = false}>Close</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"
+                        on:click={ () => showEditPopup = false}>Close
+                </button>
                 <button type="button" class="btn btn-primary" on:click={() => updateConverter()}>Save changes</button>
             </div>
         </form>
@@ -160,9 +180,10 @@
 <div class="container">
     <div class="table-wrapper">
         <div class="col-md-6">
-            <button class=" btn btn-success" type="button"  on:click={ () => showAddPopup = true}>Add a new converter</button>
+            <button class=" btn btn-success" type="button" on:click={ () => showAddPopup = true}>Add a new converter
+            </button>
         </div>
-        <table class="table table-hover ; table table-striped" >
+        <table class="table table-hover ; table table-striped">
             <thead>
             <tr>
                 <th scope="col">Converter_id</th>
@@ -174,36 +195,38 @@
             </thead>
             <tbody>
             {#each $apiData as Converter}
-            <tr>
-                <th scope="row">{Converter.converter_id}</th>
-                <td>{Converter.owner_id}</td>
-                <td>{Converter.installer_id}</td>
-                <td>{Converter.expected_throughput}</td>
-                <td>
-                    <button class="bi bi-trash3-fill ; btn btn-danger" type="button"  on:click={deleteConverter(Converter.converter_id) } ></button>
-                    <button  class="bi bi-pencil-square ; btn btn-primary" type="button" on:click={isEdit(Converter.converter_id)}></button>
-                </td>
-            </tr>
+                <tr>
+                    <th scope="row">{Converter.converter_id}</th>
+                    <td>{Converter.owner_id}</td>
+                    <td>{Converter.installer_id}</td>
+                    <td>{Converter.expected_throughput}</td>
+                    <td>
+                        <button class="bi bi-trash3-fill ; btn btn-danger" type="button"
+                                on:click={() => deleteConverter(Converter.converter_id)}></button>
+                        <button class="bi bi-pencil-square ; btn btn-primary" type="button"
+                                on:click={() =>isEdit(Converter.converter_id)}></button>
+                    </td>
+                </tr>
             {/each}
-<!--            <tr>-->
-<!--                <th scope="row">2</th>-->
-<!--                <td>Converter</td>-->
-<!--                <td>Working</td>-->
-<!--                <td>-->
-<!--                    <button class="bi bi-trash3-fill ; btn btn-danger" type="button"  on:click={ () => showDeletePopup = true}></button>-->
-<!--                    <i class="bi bi-pencil-square ; btn btn-primary"></i>-->
-<!--                </td>-->
-<!--            </tr>-->
-<!--            <tr>-->
-<!--                <th scope="row">3</th>-->
-<!--                <td>Converter</td>-->
-<!--                <td>Working</td>-->
-<!--                <td>-->
-<!--                    <button class="bi bi-trash3-fill ; btn btn-danger" type="button"  on:click={ () => showDeletePopup = true}></button>-->
-<!--                    <button class="bi bi-pencil-square ; btn btn-primary" type="button"  on:click={ () => showEditPopup = true}></button>-->
-<!--                </td>-->
-<!--            </tr>-->
-<!--            </tbody>-->
+            <!--            <tr>-->
+            <!--                <th scope="row">2</th>-->
+            <!--                <td>Converter</td>-->
+            <!--                <td>Working</td>-->
+            <!--                <td>-->
+            <!--                    <button class="bi bi-trash3-fill ; btn btn-danger" type="button"  on:click={ () => showDeletePopup = true}></button>-->
+            <!--                    <i class="bi bi-pencil-square ; btn btn-primary"></i>-->
+            <!--                </td>-->
+            <!--            </tr>-->
+            <!--            <tr>-->
+            <!--                <th scope="row">3</th>-->
+            <!--                <td>Converter</td>-->
+            <!--                <td>Working</td>-->
+            <!--                <td>-->
+            <!--                    <button class="bi bi-trash3-fill ; btn btn-danger" type="button"  on:click={ () => showDeletePopup = true}></button>-->
+            <!--                    <button class="bi bi-pencil-square ; btn btn-primary" type="button"  on:click={ () => showEditPopup = true}></button>-->
+            <!--                </td>-->
+            <!--            </tr>-->
+            <!--            </tbody>-->
         </table>
 
     </div>
